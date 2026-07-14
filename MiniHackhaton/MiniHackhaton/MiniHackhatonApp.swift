90cf7ae0a7224183157030ceb98ad892ba6bcbb1
Alvin Reyvaldo <44659181+alvinalibaba1@users.noreply.github.com>
2026-07-14 15:27:52 +0700

    feat/improve-gitignore

---
+++ MiniHackhaton/MiniHackhaton/MiniHackhatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniHackhatonApp.swift
+//  MiniHackhaton
+//
+//  Created by Training-26 on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniHackhatonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
